ae32f2697b6916ef35eebba14e2d2eaabbd994b0
wnshan <qwerdf999@proton.me>
2025-05-28 16:36:32 +0700

    手动addWidget添加

--- GGlobal.ts
+++ GGlobal.ts
@@ -1,4 +1,4 @@
-import { Button, Node } from "cc";
+import { Button, Node, Widget } from "cc";
 import { GAudio } from "./core/game/GAudio";
 import { LangLabel } from "./core/game/lang/LangLabel";
 import { LangSprite } from "./core/game/lang/LangSprite";
@@ -71,6 +71,39 @@ export namespace GG {
             if (ls) {
                 ls.langKey = key;
             }
+        }
+
+        /**
+         * 添加Widget组件(项目切换分辨率时会自动改变尺寸，手动添加)
+         * @param node 节点
+         * @param dis 对齐方式
+         */
+        public static addWidget(
+            node: Node,
+            dis?: {
+                top?: number;
+                left?: number;
+                right?: number;
+                bottom?: number;
+                isAlignBottom?: boolean;
+                isAlignLeft?: boolean;
+                isAlignRight?: boolean;
+                isAlignTop?: boolean;
+            }
+        ): void {
+            let widget = node.getComponent(Widget);
+            if (!widget) {
+                widget = node.addComponent(Widget);
+            }
+            widget.alignMode = Widget.AlignMode.ON_WINDOW_RESIZE;
+            widget.isAlignBottom = dis?.isAlignBottom ?? true;
+            widget.isAlignLeft = dis?.isAlignLeft ?? true;
+            widget.isAlignRight = dis?.isAlignRight ?? true;
+            widget.isAlignTop = dis?.isAlignTop ?? true;
+            widget.top = dis?.top ?? 0;
+            widget.left = dis?.left ?? 0;
+            widget.right = dis?.right ?? 0;
+            widget.bottom = dis?.bottom ?? 0;
         }
     }
 }
