99e00ddbc868f8cdace3c3f759685cded8cdf4ff
wnshan <qwerdf999@proton.me>
2025-06-12 12:36:33 +0700

    bundle version添加

--- core/ResMgr.ts
+++ core/ResMgr.ts
@@ -13,13 +13,13 @@ import {
 } from "cc";
 
 export namespace ResLoad {
-    const getBundle = (bName: string): Promise<AssetManager.Bundle> => {
+    const getBundle = (bName: string, version: string | null = null): Promise<AssetManager.Bundle> => {
         return new Promise((resolve) => {
             let db = assetManager.getBundle(bName);
             if (db) {
                 resolve(db);
             } else {
-                assetManager.loadBundle(bName, null, (err: Error, data: AssetManager.Bundle) => {
+                assetManager.loadBundle(bName, { version }, (err: Error, data: AssetManager.Bundle) => {
                     if (err) {
                         resolve(null);
                     } else {
@@ -30,9 +30,14 @@ export namespace ResLoad {
         });
     };
 
-    const res = <T extends Asset>(bName: string, resName: string, resType: typeof Asset): Promise<T> => {
+    const res = <T extends Asset>(
+        bName: string,
+        resName: string,
+        resType: typeof Asset,
+        version: string | null = null
+    ): Promise<T> => {
         return new Promise(async (resolve) => {
-            let bundle = await getBundle(bName);
+            let bundle = await getBundle(bName, version);
             bundle.load(resName, resType, null, (err: Error, data: T) => {
                 if (err) {
                     resolve(null);
@@ -43,9 +48,14 @@ export namespace ResLoad {
         });
     };
 
-    export const dir = <T extends Asset>(bName: string, resName: string, onProgress?: Function): Promise<T[]> => {
+    export const dir = <T extends Asset>(
+        bName: string,
+        resName: string,
+        onProgress?: Function,
+        version: string | null = null
+    ): Promise<T[]> => {
         return new Promise(async (resolve) => {
-            let bundle = await getBundle(bName);
+            let bundle = await getBundle(bName, version);
             bundle.loadDir(
                 resName,
                 (finish: number, total: number, item: any) => {
@@ -66,10 +76,11 @@ export namespace ResLoad {
         bName: string,
         resName: string,
         resType: new (...args: any[]) => T,
-        onProgress?: Function
+        onProgress?: Function,
+        version: string | null = null
     ): Promise<T[]> => {
         return new Promise(async (resolve) => {
-            let bundle = await getBundle(bName);
+            let bundle = await getBundle(bName, version);
             bundle.loadDir(
                 resName,
                 resType,
@@ -87,42 +98,46 @@ export namespace ResLoad {
         });
     };
 
-    export const atlas = (bName: string, pName: string): Promise<SpriteAtlas> => {
-        return res(bName, pName, SpriteAtlas);
+    export const atlas = (bName: string, pName: string, version: string | null = null): Promise<SpriteAtlas> => {
+        return res(bName, pName, SpriteAtlas, version);
     };
 
-    export const spriteFrame = (bName: string, pName: string): Promise<SpriteFrame> => {
+    export const spriteFrame = (bName: string, pName: string, version: string | null = null): Promise<SpriteFrame> => {
         if (!pName.endsWith("spriteFrame")) {
             pName = `${pName}/spriteFrame`;
         }
-        return res(bName, pName, SpriteFrame);
+        return res(bName, pName, SpriteFrame, version);
     };
 
-    export const spineData = (bName: string, pName: string): Promise<sp.SkeletonData> => {
-        return res(bName, pName, sp.SkeletonData);
+    export const spineData = (
+        bName: string,
+        pName: string,
+        version: string | null = null
+    ): Promise<sp.SkeletonData> => {
+        return res(bName, pName, sp.SkeletonData, version);
     };
 
-    export const prefab = (bName: string, pName: string): Promise<Prefab> => {
-        return res(bName, pName, Prefab);
+    export const prefab = (bName: string, pName: string, version: string | null = null): Promise<Prefab> => {
+        return res(bName, pName, Prefab, version);
     };
 
-    export const audioClip = (bName: string, pName: string): Promise<AudioClip> => {
-        return res(bName, pName, AudioClip);
+    export const audioClip = (bName: string, pName: string, version: string | null = null): Promise<AudioClip> => {
+        return res(bName, pName, AudioClip, version);
     };
 
-    export const animClip = (bName: string, pName: string): Promise<AnimationClip> => {
-        return res(bName, pName, AnimationClip);
+    export const animClip = (bName: string, pName: string, version: string | null = null): Promise<AnimationClip> => {
+        return res(bName, pName, AnimationClip, version);
     };
 
-    export const json = (bName: string, pName: string): Promise<JsonAsset> => {
-        return res(bName, pName, JsonAsset);
+    export const json = (bName: string, pName: string, version: string | null = null): Promise<JsonAsset> => {
+        return res(bName, pName, JsonAsset, version);
     };
 
-    export const font = (bName: string, pName: string): Promise<Font> => {
-        return res(bName, pName, Font);
+    export const font = (bName: string, pName: string, version: string | null = null): Promise<Font> => {
+        return res(bName, pName, Font, version);
     };
 
-    export const bundle = (bName: string): Promise<AssetManager.Bundle> => {
-        return getBundle(bName);
+    export const bundle = (bName: string, version: string | null = null): Promise<AssetManager.Bundle> => {
+        return getBundle(bName, version);
     };
 }
