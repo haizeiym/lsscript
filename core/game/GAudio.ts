094bea92b28423432bb0b8cd0f853edc781a6065
wnshan <qwerdf999@proton.me>
2025-08-02 14:56:37 +0700

    日志添加

--- core/game/GAudio.ts
+++ core/game/GAudio.ts
@@ -4,12 +4,20 @@ import { ResLoad } from "../ResMgr";
 export namespace GAudio {
     export const bgm = (bName: string, pName: string, loop: boolean = true) => {
         ResLoad.audioClip(bName, pName).then((sound) => {
+            if (!sound) {
+                console.warn(`bgm not found: ${bName} ${pName}`);
+                return;
+            }
             AudioMgr.playBgm(sound, loop);
         });
     };
 
     export const effect = (bName: string, pName: string, volume: number = AudioMgr.effectVolume) => {
         ResLoad.audioClip(bName, pName).then((sound) => {
+            if (!sound) {
+                console.warn(`effect not found: ${bName} ${pName}`);
+                return;
+            }
             AudioMgr.playEffect(sound, volume);
         });
     };
