f39d4ad56fecb227999e2cf2501992358872ca2e
wnshan <qwerdf999@proton.me>
2025-10-18 20:58:45 +0700

    导出修改

--- core/NMgr.ts
+++ core/NMgr.ts
@@ -1,4 +1,4 @@
-import { ZipLoader } from "cccbundlezip";
+// import { ZipLoader } from "cccbundlezip";
 import UTime from "cocos-timer-util";
 import FnAsync from "fn-async-cp";
 import * as NWs from "sp-wsocket";
@@ -6,4 +6,5 @@ import * as NWs from "sp-wsocket";
 /**
  * npm导出
  */
-export { FnAsync as NCountFn, UTime as NTime, NWs, ZipLoader as NZip };
+// export { FnAsync as NCountFn, UTime as NTime, NWs, ZipLoader as NZip };
+export { FnAsync as NCountFn, UTime as NTime, NWs };
