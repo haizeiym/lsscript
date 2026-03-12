a6885e42ba97dcff43325409ec6138f49836339b
wnshan <qwerdf999@proton.me>
2026-03-12 21:15:43 +0700

    export 添加

---
+++ index.ts
@@ -0,0 +1,26 @@
+/**
+ * lean script 框架导出
+ */
+//core
+export { AnimFa, AnimSp, AnimTw } from "./core/AnimMgr";
+export { AudioMgr } from "./core/AudioMgr";
+export { BindUI } from "./core/BindUI";
+export { Btn } from "./core/BtnMgr";
+export { Events } from "./core/EventMgr";
+export { httpGet, httpPost } from "./core/HttpMgr";
+export { NCountFn, NTime, NWs } from "./core/NMgr";
+export { ResLoad } from "./core/ResMgr";
+export { Tools } from "./core/ToolsMgr";
+
+//game
+export { BaseComponent } from "./core/game/BaseComponent";
+export { eventsOnLoad, preloadEvent } from "./core/game/BaseDescriptor";
+export { GAudio } from "./core/game/GAudio";
+export { GEventName } from "./core/game/GEventsName";
+export { LangLabel } from "./core/game/lang/LangLabel";
+export { LangMgr } from "./core/game/lang/LangMgr";
+export { LangSprite } from "./core/game/lang/LangSprite";
+
+//global
+export { GG } from "./GGlobal";
+export { lsGameData } from "./GameData";
