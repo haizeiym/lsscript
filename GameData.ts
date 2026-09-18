e3dfc1e85340531a6fd35effeb9aa6c8a04e902c
wnshan <qwerdf999@proton.me>
2026-09-19 00:12:31 +0700

    默认值修改

---
+++ GameData.ts
@@ -0,0 +1,60 @@
+import { sys } from "cc";
+
+const VoiceKey = {
+    isStopAudio: "is_stopAudio",
+    isStopBgm: "is_stopBgm",
+    isStopEffect: "is_stopEffect"
+} as const;
+
+export class GameData {
+    public static setSaveData(key: string, value: any) {
+        try {
+            sys.localStorage.setItem(key, JSON.stringify(value));
+        } catch (error) {
+            sys.localStorage.setItem(key, null);
+        }
+    }
+
+    public static getSaveData(key: string, defaultValue?: any) {
+        const value = sys.localStorage.getItem(key);
+        if (value === null) {
+            return defaultValue;
+        }
+        try {
+            return JSON.parse(value);
+        } catch (error) {
+            return defaultValue;
+        }
+    }
+
+    //全局存储是否播放音乐/音效
+    public static setIsStopAudio(isStopAudio: boolean) {
+        this.setSaveData(VoiceKey.isStopAudio, isStopAudio ? 1 : 0);
+    }
+
+    public static getIsStopAudio(): boolean {
+        return this.getSaveData(VoiceKey.isStopAudio, 1) === 1;
+    }
+
+    //全局存储是否停止背景音乐
+    public static setIsStopBgm(isStopBgm: boolean) {
+        this.setSaveData(VoiceKey.isStopBgm, isStopBgm ? 1 : 0);
+    }
+
+    public static getIsStopBgm(): boolean {
+        return this.getSaveData(VoiceKey.isStopBgm, 1) === 1;
+    }
+
+    //全局存储是否停止音效
+    public static setIsStopEffect(isStopEffect: boolean) {
+        this.setSaveData(VoiceKey.isStopEffect, isStopEffect ? 1 : 0);
+    }
+
+    public static getIsStopEffect(): boolean {
+        return this.getSaveData(VoiceKey.isStopEffect, 1) === 1;
+    }
+}
+
+
+export const lsGameData = GameData;
+
